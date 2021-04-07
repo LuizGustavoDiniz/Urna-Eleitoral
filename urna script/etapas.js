58324072c6251d4cbc33511d027ef9cd908cfd21
Luiz Gustavo Gonçalves Diniz <gustavo.diniz.gon@gmail.com>
2021-04-07 18:26:11 -0300

    Urna Eletrônica teste
    urna eletrônica simples para treino de html,css e javascript

---
+++ urna script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'naruto',
+                partido: 'konoha',
+                fotos: [
+                    {url: 'naruto.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '67899',
+                nome: 'guts',
+                partido: 'berserk',
+                fotos: [
+                    { url: 'guts.jpg', legenda: 'vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'ichigo',
+                partido:'bleach',
+                vice: 'griffith',
+                fotos: [
+                    { url: 'ichigo.jpg', legenda: 'prefeito' }, { url: 'grifith.jpg', legenda: 'vice-prefeito' }
+                ]
+            },
+            {
+
+                numero: '20',
+                nome: 'luffy',
+                partido: 'onepiece',
+                vice: 'natsu',
+                fotos: [
+                    { url: 'luffy.jpg', legenda: 'prefeito' },
+                    { url: 'natsu.jpg', legenda: 'vice-prefeito' }
+                ]
+            },
+        ]
+    },
+];
